8269e5b99c5a9d11420e041fd004e23c2c827b20
devjinjin <devjinjin@gmail.com>
2021-05-20 16:01:59 +0900

    Initial Commit

---
+++ SwipeViewPager/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SwipeViewPager
+//
+//  Created by jylee-mac on 2021/05/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
